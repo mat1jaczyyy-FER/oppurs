7b201b20bb45323d47fd8cc251fc5a4c580ffc4e
mat1jaczyyy <mat1jaczyyy@gmail.com>
2023-11-13 21:48:58 +0100

    Lab1

---
+++ lab1/Lab_Timer/Core/Src/gpio.c
@@ -0,0 +1,85 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PDPin PDPin PDPin PDPin */
+  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+void gpio_led_state(uint8_t LED_ID, uint8_t state) {
+  GPIO_PinState pinState = (state == 1)? GPIO_PIN_SET : GPIO_PIN_RESET;
+
+  switch (LED_ID) {
+    case 1:
+      HAL_GPIO_WritePin(GPIOD, LD4_Pin, pinState);
+      break;
+    
+    case 2:
+      HAL_GPIO_WritePin(GPIOD, LD3_Pin, pinState);
+      break;
+      
+    case 3:
+      HAL_GPIO_WritePin(GPIOD, LD5_Pin, pinState);
+      break;
+      
+    case 4:
+      HAL_GPIO_WritePin(GPIOD, LD6_Pin, pinState);
+      break;
+  }
+}
+/* USER CODE END 2 */
